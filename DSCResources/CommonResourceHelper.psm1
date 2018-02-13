2753d26154f6093bef19c9b91c29158e6d4a4696
Anthony <anthonywaye@gmail.com>
2018-02-13 18:54:08 +1000

    localization updates

---
+++ DSCResources/CommonResourceHelper.psm1
@@ -0,0 +1,51 @@
+$errorActionPreference = 'Stop'
+Set-StrictMode -Version 'Latest'
+
+function Read-RESTResponse
+{
+    [CmdletBinding()]
+    Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory=$true,
+                   Position=0)]
+        $Response
+    )
+
+      $exception = $_.Exception.Response.GetResponseStream()
+      $reader = New-Object -TypeName System.IO.StreamReader -ArgumentList ($exception)
+      $reader.BaseStream.Position = 0
+      $reader.DiscardBufferedData()
+      $responseBody = $reader.ReadToEnd()
+      return $responseBody
+}
+
+function Get-LocalizedData
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $ResourceName
+    )
+
+    $resourceDirectory = Join-Path -Path $PSScriptRoot -ChildPath $ResourceName
+    $localizedStringFileLocation = Join-Path -Path $resourceDirectory -ChildPath $PSUICulture
+
+    if (-not (Test-Path -Path $localizedStringFileLocation))
+    {
+        # Fallback to en-US
+        $localizedStringFileLocation = Join-Path -Path $resourceDirectory -ChildPath 'en-US'
+    }
+
+    Import-LocalizedData `
+        -BindingVariable 'localizedData' `
+        -FileName "$ResourceName.strings.psd1" `
+        -BaseDirectory $localizedStringFileLocation
+
+    return $localizedData
+}
+
+Export-ModuleMember -Function @( 'Read-RESTResponse', 'Get-LocalizedData' )
---
+++ DSCResources/xDSCVault_Unwrap/en-US/xDSCVault_Unwrap.psd1
@@ -0,0 +1,7 @@
+ConvertFrom-StringData @'
+    VaultMethod = '{0}' selected 
+    VaultDnsException = Vault address could not be resolved
+    VaultTokenLookupInvalid = Wrapping token is not valid or does not exist 
+    Vaultissecretwrapped = Checking to see if secret is still wrapped
+    CheckingDNS = Checking DNS for '{0}'
+'@

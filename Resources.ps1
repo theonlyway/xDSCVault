1f4f99173e56d9a9f23c01c716cf6864f9c9004a
Anthony Waye <anthonywaye@gmail.com>
2018-02-15 12:29:07 +1000

    started vault write module

--- Resources.ps1
+++ Resources.ps1
@@ -13,3 +13,15 @@ $ApiPrefix = New-xDscResourceProperty -Name ApiPrefix -Type String -Attribute Wr
 #Now create the resource
 New-xDscResource -Name xDSCVault_Unwrap -FriendlyName VaultUnwrap -Property $WrappedToken, $Method, $VaultAddress, $cliPath, $ApiPrefix -Path 'C:\Users\Anthony\Documents\BitBucket\xDSCVault' -Force
 Test-xDscSchema -Path 'C:\Users\Anthony\Documents\BitBucket\xDSCVault\DSCResources\xDSCVault_Unwrap\xDSCVault_Unwrap.schema.mof'
+
+
+$Method = New-xDscResourceProperty -Name VaultMethod -Attribute Required -Type String -Description 'Method to perform action. CLI or API' -ValueMap 'Api', 'Cli' -Values 'Api', 'Cli'
+$VaultAddress = New-xDscResourceProperty -Name VaultPAddress -Type String -Attribute Required -Description 'Address of the Vault server'
+$cliPath = New-xDscResourceProperty -Name CliPath -Type String -Attribute Write -Description 'Path to the vault binary'
+$ApiPrefix = New-xDscResourceProperty -Name ApiPrefix -Type String -Attribute Write -Description 'Version of the API to use'
+$VaultPath = New-xDscResourceProperty -Name VaultPath -Type String -Attribute Key -Description 'Vault path to create the secret'
+$VaultValue = New-xDscResourceProperty -Name VaultValue -Type String -Attribute Required -Description 'Value for the secret'
+
+#Now create the resource
+New-xDscResource -Name xDSCVault_Write -FriendlyName VaultWrite -Property $Method, $VaultAddress, $VaultPath, $VaultValue, $cliPath, $ApiPrefix -Path 'C:\Users\Anthony\Documents\BitBucket\xDSCVault' -Force
+Test-xDscSchema -Path 'C:\Users\Anthony\Documents\BitBucket\xDSCVault\DSCResources\xDSCVault_Write\xDSCVault_Write.schema.mof'

17eb9559f5928c3eecad5950cc1fdb2a13a6682d
Anthony Waye <anthonywaye@gmail.com>
2018-02-27 12:30:09 +1000

    start of local user module

--- Resources.ps1
+++ Resources.ps1
@@ -28,3 +28,25 @@ $ForceUpdate = New-xDscResourceProperty -Name ForceUpdate -Type Boolean -Attribu
 #Now create the resource
 New-xDscResource -Name xDSCVault_Write -FriendlyName VaultWrite -Property $Method, $VaultAddress, $VaultPath, $VaultValue, $cliPath, $ApiPrefix, $ApiPath, $RandomSecret, $ForceUpdate -Path 'C:\Users\Anthony\Documents\BitBucket\xDSCVault' -Force
 Test-xDscSchema -Path 'C:\Users\Anthony\Documents\BitBucket\xDSCVault\DSCResources\xDSCVault_Write\xDSCVault_Write.schema.mof'
+
+$UserName = New-xDscResourceProperty -Name UserName -Attribute Key -Type String -Description 'Indicates the account name for which you want to ensure a specific state'
+$Description = New-xDscResourceProperty -Name Description -Attribute Write -Type String -Description 'Indicates the description you want to use for the user account'
+$Disabled = New-xDscResourceProperty -Name Disabled -Type Boolean -Attribute Required -Description 'Value used to disable/enable a user account'
+$Ensure = New-xDscResourceProperty -Name Ensure -Type String -Attribute Required -Description 'Ensures that the feature is present or absent' -ValueMap 'Present', 'Absent' -Values 'Present', 'Absent'
+$FullName = New-xDscResourceProperty -Name FullName -Type String -Attribute Write -Description 'Value used to disable/enable a user account'
+$PasswordChangeNotAllowed = New-xDscResourceProperty -Name PasswordChangeNotAllowed -Type Boolean -Attribute Required -Description 'Indicates if the user can change the password'
+$PasswordChangeRequired = New-xDscResourceProperty -Name PasswordChangeRequired -Type Boolean -Attribute Required -Description 'Indicates if the user must change the password at the next sign in'
+$PasswordNeverExpires = New-xDscResourceProperty -Name PasswordNeverExpires -Type Boolean -Attribute Required -Description 'Indicates if the password will expire'
+$VaultPath = New-xDscResourceProperty -Name VaultPath -Type String -Attribute Key -Description 'Vault path to create the secret'
+$VaultAddress = New-xDscResourceProperty -Name VaultAddress -Type String -Attribute Required -Description 'Address of the Vault server'
+$VaultValue = New-xDscResourceProperty -Name VaultValue -Type String -Attribute Write -Description 'Value for the secret'
+$Method = New-xDscResourceProperty -Name VaultMethod -Attribute Required -Type String -Description 'Method to perform action. CLI or API' -ValueMap 'Api', 'Cli' -Values 'Api', 'Cli'
+$ApiPrefix = New-xDscResourceProperty -Name ApiPrefix -Type String -Attribute Write -Description 'Version of the API to use'
+$ApiPath = New-xDscResourceProperty -Name ApiPath -Type String -Attribute Read -Description 'Complete API path'
+$cliPath = New-xDscResourceProperty -Name CliPath -Type String -Attribute Write -Description 'Path to the vault binary'
+
+
+#Now create the resource
+New-xDscResource -Name xDSCVault_LocalUser -FriendlyName VaultLocalUser -Property $Method, $VaultAddress, $VaultPath, $VaultValue, $cliPath, $ApiPrefix, $ApiPath, `
+    $UserName, $Description, $Disabled, $Ensure, $FullName, $PasswordChangeNotAllowed, $PasswordChangeRequired, $PasswordNeverExpires -Path 'C:\Users\Anthony\Documents\BitBucket\xDSCVault' -Force
+Test-xDscSchema -Path 'C:\Users\Anthony\Documents\BitBucket\xDSCVault\DSCResources\xDSCVault_LocalUser\xDSCVault_LocalUser.schema.mof'
